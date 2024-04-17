130414129d2f3752c6473ddff95a64a5adcd74a9
raredietcokew <167277666+raredietcokew@users.noreply.github.com>
2024-04-17 00:40:07 -0500

    Powershell.ps1

--- Powershell.ps1
+++ Powershell.ps1
@@ -1,14 +1,20 @@
+# Prompt the user to enter their username
+$username = Read-Host -Prompt "Enter your username"
+
 # Prompt the user to enter a password
 $password = Read-Host -Prompt "Enter your password" -AsSecureString
 
 # Convert the secure string password to plain text
 $plaintextPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
 
-# Specify the file path where you want to save the password on the desktop
+# Specify the file path where you want to save the credentials on the desktop
 $desktopPath = [System.IO.Path]::Combine($env:USERPROFILE, "Desktop")
-$filePath = Join-Path -Path $desktopPath -ChildPath "Password.txt"
+$filePath = Join-Path -Path $desktopPath -ChildPath "Credentials.txt"
 
-# Write the password to a plain text file
-$plaintextPassword | Out-File -FilePath $filePath -Encoding ASCII
+# Create a credential object with the username and password
+$credential = New-Object System.Management.Automation.PSCredential ($username, $password)
 
-Write-Host "Password saved to $filePath."
+# Export the credential object to a text file
+$credential | Export-Clixml -Path $filePath
+
+Write-Host "Credentials saved to $filePath."
