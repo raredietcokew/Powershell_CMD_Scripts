8bf5299c68dd5216b54edd13488d892d8103f1ba
raredietcokew <167277666+raredietcokew@users.noreply.github.com>
2024-04-17 18:06:24 -0500

    Update Powershell.ps1

--- Powershell.ps1
+++ Powershell.ps1
@@ -7,9 +7,8 @@ $password = Read-Host -Prompt "Enter your password" -AsSecureString
 # Convert the secure string password to plain text
 $plaintextPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
 
-# Specify the file path where you want to save the credentials on the desktop
-$desktopPath = [System.IO.Path]::Combine($env:USERPROFILE, "Desktop")
-$filePath = Join-Path -Path $desktopPath -ChildPath "Credentials.txt"
+# Specify the file path where you want to save the credentials on the D:\ drive
+$filePath = "D:\Credentials.txt"
 
 # Create a string containing the username and password
 $credentialString = "Username: $username`nPassword: $plaintextPassword"
